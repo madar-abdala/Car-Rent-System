77fc2b97a37bf3987c9b81806b532bdca0667b9f
Madar-abdala <abdalaabdinur@gmail.com>
2026-03-13 19:49:11 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace CarRentSeystem_QEYS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.crs = new CircularProgressBar.CircularProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // crs
+            // 
+            this.crs.AnimationFunction = WinFormAnimation.KnownAnimationFunctions.Liner;
+            this.crs.AnimationSpeed = 500;
+            this.crs.BackColor = System.Drawing.Color.Transparent;
+            this.crs.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold);
+            this.crs.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.crs.InnerColor = System.Drawing.Color.Navy;
+            this.crs.InnerMargin = 2;
+            this.crs.InnerWidth = -1;
+            this.crs.Location = new System.Drawing.Point(139, 23);
+            this.crs.MarqueeAnimationSpeed = 2000;
+            this.crs.Name = "crs";
+            this.crs.OuterColor = System.Drawing.Color.Maroon;
+            this.crs.OuterMargin = -25;
+            this.crs.OuterWidth = 26;
+            this.crs.ProgressColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.crs.ProgressWidth = 25;
+            this.crs.SecondaryFont = new System.Drawing.Font("Microsoft Sans Serif", 36F);
+            this.crs.Size = new System.Drawing.Size(281, 260);
+            this.crs.StartAngle = 270;
+            this.crs.SubscriptColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(166)))), ((int)(((byte)(166)))));
+            this.crs.SubscriptMargin = new System.Windows.Forms.Padding(10, -35, 0, 0);
+            this.crs.SubscriptText = "";
+            this.crs.SuperscriptColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(166)))), ((int)(((byte)(166)))));
+            this.crs.SuperscriptMargin = new System.Windows.Forms.Padding(10, 35, 0, 0);
+            this.crs.SuperscriptText = "";
+            this.crs.TabIndex = 0;
+            this.crs.TextMargin = new System.Windows.Forms.Padding(8, 8, 0, 0);
+            this.crs.Value = 68;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Fuchsia;
+            this.ClientSize = new System.Drawing.Size(589, 315);
+            this.Controls.Add(this.crs);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CircularProgressBar.CircularProgressBar crs;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CarRentSeystem;
+
+namespace CarRentSeystem_QEYS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            crs.Value = 0;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            crs.Value += 1;
+            crs.Text = crs.Value.ToString() + "%";
+            if (crs.Value == 100)
+            {
+                timer1.Stop();
+                UsersForm u = new UsersForm();
+                this.Hide();
+                u.Show();
+            }
+        }
+    }
+}
